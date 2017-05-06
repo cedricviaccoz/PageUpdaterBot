accb01a8b3f6a5ef553a1805c6c82e8ba279013e
Cédric Viaccoz <cedric.viaccoz@gmail.com>
2017-05-07 00:25:05 +0200

    PUB skeleton : phase 1

--- PageUpdaterBot.py
+++ PageUpdaterBot.py
@@ -8,6 +8,8 @@ user='PageUpdaterBot'
 passw='hqk-NGF-S6z-qqF'
 baseurl='http://wikipast.epfl.ch/wikipast/'
 summary='Wikipastbot update'
+#Page contenant les méta information de PUB, notamment son compteur d'IDs.
+HUBPage='http://wikipast.epfl.ch/'
 
 # Login request
 payload={'action':'query','format':'json','utf8':'','meta':'tokens','type':'login'}
@@ -35,8 +37,151 @@ for primitive in soup.findAll("text"):
     code+=primitive.string
 print(code)
 
-#TODO main loop
+main()
+#test()
 
+
+# Définis à part du code du main, chaque fonction
+# devrait idéalement être testée, si possible ici.
+# mais après on fait comme on veut.
+def test():
+	#TODO
+	pass
+
+#--------- main code-------------------
+def main():
+	# Récupération de l'Id de base pour les PUB_id
+	PUBId = fetchPUBmetaInfo(HUBPage)
+
+	# Récupération de la liste de pages à parcourir.
+	pagesToMod = []
+	if PUBId == 0:
+		pagesToMod = getPageList(True)
+	else:
+		pagesToMod = getPageList(False)
+
+	## boucle d'action principale du code.
+	for u in pagesToMod:
+		contenu = fetchPageData(u)
+		pageTitle = getTitle(contenu)
+		allEntries = parseEntries(contenu)
+
+		for entry in allEntries:
+			
+
+
+	#TODO
+
+	updatePUBmetaInfo(HUBPage, PUBId)
+
+
+# Cette fonction va s'occuper d'aller sur la page
+# indiquée en argument et regarder si elle trouve le bloc
+# de méta information de PUB (bloc commençant par 
+# "<!-- PUB METAINFOS :").
+# Si ce bloc de méta info
+# est présent, elle va retourner l'Id qui y 
+# est contenu (représentant le dernier Id attribué
+# à une entrée de page).
+# Si ce bloc de méta info n'est pas présent,
+# cette fonction va s'occuper de le créer,
+# y mettre comme valuer d'ID 0, puis retourner
+# 0. (forcément)
+# Si une quelquonque erreur s'est produite,
+# la fonction retournera un chiffre négatif
+# ou bien enverra une exception (comme vous le sentez)
+#
+# @param page: String 
+#			   la page ou aller chercher les métaInfo.
+def fetchPUBmetaInfo(page):
+	#TODO
+	pass
+
+# Cette fonction doit être appelée après que 
+# PUB ait fait toute sa traversée, elle doit
+# actualiser l'ID contenu de méta_info.
+# Si pour une quelconque raison le bloc
+# méta info n'existe pas, la fonction doit le recréer
+# et l'initialiser avec le correct ID.
+#
+# @param page : String
+#				la page ou aller chercher les métaInfo.
+# @param newId : Int 
+#				Le nouvel ID a inscrire dans les metaInfo.
+def updatePUBmetaInfo(page, newId):
+	#TODO
+	pass
+
+'''
+En fonction de la variable fromScratch,
+cette fonction va récupérer la liste des pages qui
+ont été modifiées récemment, ou bien
+toutes les pages sur lequel on veut établir
+un indexage par PUBId.
+Elle retournera la liste des pages sous
+la forme d'une liste d'url wikipast.
+
+@para fromScratch : Boolean
+				 vaut true si on veut faire un indexage depuis 0
+				 ou false si on veut juste récupérer les pages
+				 récemment modifiées.
+'''
+def getPageList(fromScratch):
+	#TODO
+	pass
+
+'''
+A l'aide de l'url donné en argument,
+va récupérer les données de cette page,
+sous le format qui vous plaît (JSON je suppose ?)
+
+@param pageUrl : String
+				l'url de la page wikipast ou aller chercher les données.
+'''
+def fetchPageData(pageUrl):
+	#TODO
+	pass
+
+'''
+va bêtement récupérer le titre
+de la page courante et le renvoyer
+sous forme de String.
+
+@param content : ???? (à voir)
+				le contenu de la page
+'''
+def getTitle(content):
+	#TODO
+	pass
+
+'''
+Va s'occuper de trier le contenu
+de la page donné en argument
+pour en ressortir que les entrées biographies
+sourcées (il faudra donc éviter si ils on rajouté
+du texte autre part qui n'a rien à voir).
+Une entrée est normalement écrite sous la forme (dans le code):
+* [[YYYY.MM.JJ]] / [[<lieu>]]. [[<évènement>]] entre [[<Mr. X>]] et [[<Mr.Y>]]. [<référence>]
+
+!! remarque importante que je na'vais pas pensé avant :
+Où met-on le PUB_id ? (il sera de la forme "<!-- PUB_id=69-->") ?
+Il faut penser qu'on doit le mettre dans un endroit stratégique,
+car il faudrait que si d'autres bots reprennent ou modifient des entrées,
+ils conservent l'intégrité du PUB_id.
+
+Cette fonction retournera une liste de String qui constituent
+les entrées retournées sous la forme montrée plus haut.
+
+@param content : ???? (à voir)
+				le contenu de la page
+'''
+def parseEntries(content):
+	#TODO
+	pass
+
+
+
+## garbage, kept for historical reason.
 
 # for name in names:
 #     result=requests.post(baseurl+'api.php?action=query&titles='+name+'&export&exportnowrap')
