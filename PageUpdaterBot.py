9a75978498af247c6682a3f81842ad397f2c2b43
matteogiorla <matteo.giorla@me.com>
2017-05-07 14:30:02 +0200

    areEntriesSimilar implementation

--- PageUpdaterBot.py
+++ PageUpdaterBot.py
@@ -106,46 +106,78 @@ def main():
 	updatePUBmetaInfo(HUBPage, PUBId)
 
 
-# Cette fonction va s'occuper d'aller sur la page
-# indiquée en argument et regarder si elle trouve le bloc
-# de méta information de PUB (bloc commençant par 
-# "<!-- PUB METAINFOS :").
-# Si ce bloc de méta info
-# est présent, elle va retourner l'Id qui y 
-# est contenu (représentant le dernier Id attribué
-# à une entrée de page).
-# Si ce bloc de méta info n'est pas présent,
-# cette fonction va s'occuper de le créer,
-# y mettre comme valuer d'ID 0, puis retourner
-# 0. (forcément)
-# Si une quelquonque erreur s'est produite,
-# la fonction retournera un chiffre négatif
-# ou bien enverra une exception (comme vous le sentez)
-#
-def fetchPUBmetaInfo():
-	HUBcontent = fetchPageData(user)
-	result = re.search(beginID + '(.*)' + endID, HUBcontent)
-	result = result.group(1)
-	if result == None: #TODO si les balises ont été modifiées
+
+'''
+Récupère le PUB_id
+dans le contenu passé en argument.
+(Un PUB_id a la forme suivante :
+"<!-- PUB_id = &beginID&69&endID& -->")
+S'il n'y en a pas, retourne None,
+sinon retourne le PUBId (en string).
+S'il y en a plusieurs, il retourne le dernier.
+
+@param content : String
+			  le contenu dans lequel trouver l'id.
+'''
+def getPUBId(content):
+	return re.search(beginID + '(.*)' + endID, content).group(1)
+
+'''
+Récupère tous les PUB_id
+dans le contenu passé en argument.
+(Un PUB_id a la forme suivante :
+"<!-- PUB_id = &beginID&69&endID& -->")
+S'il n'y en a pas, retourne un tableau vide.
+
+@param content : String
+			  le contenu dans lequel trouver les ids.
+'''
+def getAllPUBIds(content):
+	return re.findall(beginID + '(.*)' + endID, content)
+
+
+'''
+Cette fonction va s'occuper d'aller sur la page
+indiquée en argument et regarder si elle trouve le bloc
+de méta information de PUB (bloc commençant par 
+"<!-- PUB METAINFOS :").
+Si ce bloc de méta info
+est présent, elle va retourner l'Id qui y 
+est contenu (représentant le dernier Id attribué
+à une entrée de page).
+Si ce bloc de méta info n'est pas présent,
+cette fonction va s'occuper de le créer,
+y mettre comme valuer d'ID 0, puis retourner
+0.
+Si une quelquonque erreur s'est produite,
+la fonction retournera un chiffre négatif
+ou bien enverra une exception (comme vous le sentez)
+'''
+def fetchPUBmetaInfo(initialPass):
+	if initialPass:
 		currentID = '0'
-		#write metainfo to HUB
+		#écrire metainfo dans le HUB
 		newMetaInfo = metaInfo + currentID + endID
 		newContent = newMetaInfo + '\n'
 		payload={'action':'edit','assert':'user','format':'json','utf8':'','prependtext':newContent,'summary':summary,'title':user,'token':edit_token}
 		r4=requests.post(baseurl+'api.php',data=payload,cookies=edit_cookie)
 	else:
-		currentID = result
-
-
-	#TODO erreur ??
+		HUBcontent = fetchPageData(user)
+		result = getPUBId(HUBcontent)
+		if result == None:
+			#la balise a été effacée/endommagée
+			currentID = '-1'
+		else:
+			currentID = result
 	return currentID
 
-# Cette fonction doit être appelée après que 
-# PUB ait fait toute sa traversée, elle doit
-# actualiser l'ID contenu de méta_info.
-#
-# @param newId : Int 
-#				Le nouvel ID a inscrire dans les metaInfo.
+'''Cette fonction doit être appelée après que 
+PUB ait fait toute sa traversée, elle doit
+actualiser l'ID contenu de méta_info.
+
+@param newId : Int 
+				Le nouvel ID a inscrire dans les metaInfo.
+'''
 def updatePUBmetaInfo(newId):
 	result=requests.post(baseurl+'api.php?action=query&titles='+user+'&export&exportnowrap')
 	soup=BeautifulSoup(result.text,'html.parser')
@@ -191,7 +223,7 @@ def getPageList(fromScratch):
 
 '''
 À l'aide du titre de la page donné en argument,
-va récupérer les données de cette page, 
+récupère les données de cette page, 
 sous la forme d'une string
 
 @param pageName : String
@@ -210,13 +242,12 @@ def fetchPageData(pageName):
 '''
 Va s'occuper de trier le contenu
 de la page donné en argument
-pour en ressortir que les entrées biographies
-sourcées (il faudra donc éviter si ils on rajouté
-du texte autre part qui n'a rien à voir).
+pour en ressortir que les entrées biographiques
+sourcées (le texte ajouté autre part est ignoré).
 Une entrée est normalement écrite sous la forme (dans le code):
 * [[YYYY.MM.JJ]] / [[<lieu>]]. [[<évènement>]] entre [[<Mr. X>]] et [[<Mr.Y>]]. [<référence>]
 
-!! remarque importante que je na'vais pas pensé avant :
+!! remarque importante que je n'avais pas pensé avant :
 Où met-on le PUB_id ? (il sera de la forme "<!-- PUB_id=69-->") ?
 Il faut penser qu'on doit le mettre dans un endroit stratégique,
 car il faudrait que si d'autres bots reprennent ou modifient des entrées,
@@ -225,26 +256,10 @@ ils conservent l'intégrité du PUB_id.
 Cette fonction retournera une liste de String qui constituent
 les entrées retournées sous la forme montrée plus haut.
 
-@param content : ???? (à voir)
+@param content : String
 				le contenu de la page
 '''
 def parseEntries(content):
-	#TODO
-	pass
-
-
-'''
-va récupérer le PUB_id
-contenu dans cette entrée.
-(Pour rappel, un PUB_id a comme forme :
-"<!-- PUB_id=69 -->")
-Si il n'y en a pas, retourne None,
-sinon retourn le PUBId.(un int donc)
-
-@param entry : String
-			  l'entrée biographie.
-'''
-def getPUBId(entry):
 	#TODO
 	pass
 
@@ -305,8 +320,17 @@ autrement Talse.
 				La seconde entrée avec laquelle on compare la première
 '''
 def areEntrySimilar(entry1, entry2):
-	#TODO
-	pass
+	hyperWordPattern = '[[' + '(.*)' + ']]'
+	linkPattern = '[' + '(.*)' + ']'
+	
+	#la liste des hypermots inclus également la date
+	listOfHyperwords1 = re.findall(hyperWordPattern, entry1)
+	listOfHyperwords2 = re.findall(hyperWordPattern, entry2)
+
+	listOfReferences1 = re.findall(linkPattern, entry1)
+	listOfReferences2 = re.findall(linkPattern, entry2)
+	
+	return (listOfHyperwords1 == listOfHyperwords2) and (listOfReferences1 == listOfReferences2)
 
 '''
 Va transformer une liste d'entrée
@@ -340,4 +364,5 @@ selon comment il faut uploader des modifications sur wikipast
 def uploadModifications(content, url):
 	#TODO 
 	pass
+
 
